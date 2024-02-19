163474b0196cfa548cd165c32c7d7a301fb57a64
Muaz <muazzia17@gmail.com>
2024-02-19 21:58:27 +0500

    added migrations

--- routes/Course/course.js
+++ routes/Course/course.js
@@ -1,13 +1,14 @@
 const express = require('express')
 const { getAllCourses, getASpecificCourse, deleteASpecificCourse, createCourse } = require('../../controller/Course/course')
 const { handleCourseUpload } = require('../../middleware/multer')
-const courseRouter = express.Router()
 
+const courseRouter = express.Router()
 
 courseRouter.get('/', getAllCourses)
 courseRouter.get('/:id', getASpecificCourse)
 courseRouter.delete('/:id', deleteASpecificCourse)
 courseRouter.post('/', handleCourseUpload, createCourse)
+
 // courseRouter.put('/:id')
 
 module.exports = courseRouter
