f05db005d18537547e6d0aa3641468de34546367
Muaz <muazzia17@gmail.com>
2024-03-01 22:23:06 +0500

    created two routes to verify and resend mail

--- routes/Auth/auth.js
+++ routes/Auth/auth.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const authRoutes = express.Router();
-const { checkJWT} = require("../../middleware/authenticationMiddleware");
-const { registerUser, loginUser, googleLoginPage, authGoogle, googleCallback} = require("../../controller/Auth/authController");
+const { checkJWT } = require("../../middleware/authenticationMiddleware");
+const { registerUser, loginUser, googleLoginPage, authGoogle, googleCallback, verifyEmail, resendEmail } = require("../../controller/Auth/authController");
+const checkExistingToken = require("../../middleware/previousToken");
 
 
 authRoutes.route("/register-user").post(registerUser);
+authRoutes.route('/verify-email').get(verifyEmail)
+authRoutes.route('/resend-email').get(resendEmail)
+
 authRoutes.route("/login").post(loginUser);
 authRoutes.route("/google/login").get(googleLoginPage);
 authRoutes.route("/auth/google").get(authGoogle);
