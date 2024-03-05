ed4b213baf0d61d61690c538e35e3ac5ca107705
Muaz <muazzia17@gmail.com>
2024-03-05 19:17:47 +0500

    remove unused lib

--- routes/Auth/auth.js
+++ routes/Auth/auth.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const authRoutes = express.Router();
-const { checkJWT } = require("../../middleware/authenticationMiddleware");
 const { registerUser, loginUser, googleLoginPage, authGoogle, googleCallback, verifyEmail, resendEmail } = require("../../controller/Auth/authController");
-const checkExistingToken = require("../../middleware/previousToken");
 
 
 authRoutes.route("/register-user").post(registerUser);
