4c5f44c64ba1914f106ec2756eb4967c754babb2
Muaz <muazzia17@gmail.com>
2024-03-15 21:14:56 +0500

    refactored response object

--- controller/Course/course.js
+++ controller/Course/course.js
@@ -1,16 +1,76 @@
 const { validateCreateCourse } = require('../../joiSchemas/Course/course');
-const courseModel = require('../../models/courseModel')
+const courseModel = require('../../models/courseModel');
+const userModel = require('../../models/userModel');
 const { uploadToCloudinary } = require('../../utils/cloudinary/cloudinary');
 const { responseObject } = require('../../utils/responseObject');
 
 
+const modifyArray = async (courses) => {
+    try {
+        const arr = await Promise.all(
+            courses.map(async (course, i) => {
+                try {
+                    const user = await userModel.findByPk(course.authorEmail)
+                    if (!user) return null; // Return null if user not found
+                    const { firstName, lastName, email, profilePic, coverPic } = user
+
+                    return {
+                        ...course.dataValues,
+                        images: JSON.parse(course.dataValues.images),
+                        user: {
+                            firstName,
+                            lastName,
+                            email,
+                            profilePic,
+                            coverPic
+                        }
+                    };
+                } catch (error) {
+                    console.error(error);
+                    return null;
+                }
+            })
+        );
+        return arr.filter(Boolean); // Filter out null values
+    } catch (error) {
+        console.error(error);
+        return [];
+    }
+}
+
+const modifySinlge = async (course) => {
+    try {
+        const user = await userModel.findByPk(course.authorEmail)
+        if (!user) return {}
+
+        const { firstName, lastName, coverPic, profilePic, email } = user
+        return {
+            ...course.dataValues,
+            user: {
+                firstName,
+                lastName,
+                coverPic,
+                profilePic,
+                email
+            }
+        }
+    } catch (error) {
+        console.log(error);
+        return {}
+    }
+
+}
+
 const getAllCourses = async (req, res) => {
     try {
-        const courses = await courseModel.findAll();
+        const course = await courseModel.findAll();
 
-        return res.send(responseObject('Successfull', 200, courses))
+        const data = await modifyArray(course)
+        console.log(data);
+        return res.send(responseObject('Successfull', 200, data))
 
     } catch (error) {
+        console.log(error);
         return res.status(500).send(responseObject('Server Error', 500))
     }
 }
@@ -22,7 +82,8 @@ const getASpecificCourse = async (req, res) => {
 
         if (!course) return res.status(404).send(responseObject('Course Not Found', 404))
 
-        return res.send(responseObject('Successful', 200, course))
+        const data = await modifySinlge(course)
+        return res.send(responseObject('Successful', 200, data))
     } catch (error) {
         return res.status(500).send(responseObject('Server Error', 500))
     }
@@ -39,8 +100,9 @@ const deleteASpecificCourse = async (req, res) => {
 
         if (!course) return res.status(404).send(responseObject('Course Not Found', 404))
 
+        const data = await modifySinlge(course)
         await course.destroy()
-        return res.send(responseObject("Deleted Successfully", 200, course))
+        return res.send(responseObject("Deleted Successfully", 200, data))
     } catch (error) {
         return res.status(500).send({ message: 'Server Error' })
     }
@@ -68,7 +130,9 @@ const createCourse = async (req, res) => {
         const course = await courseModel.create({ ...value, authorEmail: userEmail, images: imageUrls })
 
         if (!course) return res.status(404).send(responseObject("Course not created", 404))
-        return res.send(responseObject("Course Created Successfully", 200, course))
+
+        const data = await modifySinlge(course)
+        return res.send(responseObject("Course Created Successfully", 200, data))
     } catch (error) {
         console.log(error);
         return res.status(500).send(responseObject('Server Error', 500))
