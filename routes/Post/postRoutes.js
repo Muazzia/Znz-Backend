5d3fd49a9dd67739369ce20518b7529f04511850
Muaz <muazzia17@gmail.com>
2024-04-09 21:14:33 +0500

    updated post routes

--- routes/Post/postRoutes.js
+++ routes/Post/postRoutes.js
@@ -1,12 +1,11 @@
 const express = require("express");
 const postRouter = express.Router();
 const { addingPost, myPost, allPosts, delPost, userPost } = require("../../controller/Post/postController");
-// const { upload } = require("../../middleware/multer");
-const { handleFileUpload } = require("../../middleware/multer")
+const { handleMulterUpload } = require("../../middleware/multer")
 const checkExistingToken = require("../../middleware/previousToken");
 const { checkJWT } = require("../../middleware/authenticationMiddleware");
 
-postRouter.route("/add-post").post(checkExistingToken, checkJWT, handleFileUpload, addingPost);
+postRouter.route("/add-post").post(checkExistingToken, checkJWT, handleMulterUpload("images", false, 10), addingPost);
 postRouter.route("/my-posts").get(checkExistingToken, checkJWT, myPost);
 postRouter.route('/all-posts').get(checkExistingToken, checkJWT, allPosts)
 postRouter.route('/del-post/:id').delete(checkExistingToken, checkJWT, delPost)
@@ -16,7 +15,7 @@ postRouter.get("/", allPosts)
 postRouter.get("/my-posts", myPost);
 postRouter.get('/user-posts/:email', userPost)
 
-postRouter.post("/", handleFileUpload, addingPost);
+postRouter.post("/", handleMulterUpload("images", false, 10), addingPost);
 postRouter.delete("/:id", delPost)
 
 
