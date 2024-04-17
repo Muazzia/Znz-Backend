e2eb200e2b1cb597c4e5bff414461243535b1ec6
Muaz <muazzia17@gmail.com>
2024-04-17 19:47:34 +0500

    added route index file

--- index.js
+++ index.js
@@ -15,6 +15,8 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 
 
+
+
 // database connection called here
 require("./database/connection");
 // associations
@@ -25,37 +27,8 @@ require("./utils/passport/passport");
 require('./utils/cronjob/cronjob')
 
 
-// routes import
-const authR = require("./routes/Auth/auth");
-const adminR = require('./routes/Admin/admin')
-const userR = require("./routes/User/userRoutes");
-const storyR = require('./routes/Story/story')
-const postR = require("./routes/Post/postRoutes");
-const likeR = require("./routes/Like/likeRoutes");
-const postCommentR = require('./routes/PostComment/postComment')
-const tokenR = require('./routes/Token/token')
-const coursesR = require('./routes/Course/course')
-const followerR = require('./routes/Follower/follower')
-const productR = require('./routes/Product/product')
-
-const { checkJWT, adminCheckJWT } = require("./middleware/authenticationMiddleware");
-const checkExistingToken = require("./middleware/previousToken");
-
-//  middlewares
-app.use('/api/validatetoken', checkExistingToken, checkJWT, tokenR)
-app.use("/api/auth/user", authR);
-app.use('/api/admin', adminCheckJWT, adminR)
-app.use("/api/user", userR);
-
-app.use("/api/user/story", checkExistingToken, checkJWT, storyR);
-
-app.use("/api/user/post", checkExistingToken, checkJWT, postR);
-app.use("/api/user/post/like", checkExistingToken, checkJWT, likeR);
-app.use('/api/user/post/comment', checkExistingToken, checkJWT, postCommentR)
-app.use('/api/user/connection', checkExistingToken, checkJWT, followerR)
-
-app.use('/api/course', checkExistingToken, checkJWT, coursesR)
-app.use('/api/product', checkExistingToken, checkJWT, productR)
+const routes = require('./routes');
+app.use('/api', routes)
 
 
 // server
---
+++ routes/index.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const { checkJWT, adminCheckJWT } = require("../middleware/authenticationMiddleware");
+const checkExistingToken = require("../middleware/previousToken");
+
+const router = express.Router();
+
+const authR = require("../routes/Auth/auth");
+const adminR = require('../routes/Admin/admin')
+const userR = require("../routes/User/userRoutes");
+const storyR = require('../routes/Story/story')
+const postR = require("../routes/Post/postRoutes");
+const likeR = require("../routes/Like/likeRoutes");
+const postCommentR = require('../routes/PostComment/postComment')
+const tokenR = require('../routes/Token/token')
+const coursesR = require('../routes/Course/course')
+const followerR = require('../routes/Follower/follower')
+const productR = require('../routes/Product/product')
+
+
+
+
+
+
+router.use('/validatetoken', checkExistingToken, checkJWT, tokenR)
+router.use("/auth/user", authR);
+router.use('/admin', adminCheckJWT, adminR)
+router.use("/user", userR);
+
+router.use("/user/story", checkExistingToken, checkJWT, storyR);
+
+router.use("/user/post", checkExistingToken, checkJWT, postR);
+router.use("/user/post/like", checkExistingToken, checkJWT, likeR);
+router.use('/user/post/comment', checkExistingToken, checkJWT, postCommentR)
+router.use('/user/connection', checkExistingToken, checkJWT, followerR)
+
+router.use('/course', checkExistingToken, checkJWT, coursesR)
+router.use('/product', checkExistingToken, checkJWT, productR)
+
+
+
+module.exports = router
